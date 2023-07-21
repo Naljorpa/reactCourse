96e7033226d23f232b8589ce380c0f9a1f753aa6
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-07-21 15:05:35 -0400

    added form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css';
@@ -5,13 +7,21 @@ import './ExpenseItem.css';
 //object destructuring instead of props in ()
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+    };
+
     return (
         <Card className="expense-item">
-            <ExpenseDate date = {props.date} />
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
