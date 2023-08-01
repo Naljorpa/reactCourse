1af760f7a07f01b99e09de06a9e9e4912cf6c921
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-08-01 06:37:45 -0400

    change nomenclature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,14 +8,15 @@ import './Expenses.css';
 const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('2020');
-    const addFilterHandler = selectedYear => {
+    
+    const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     }
 
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={filteredYear} onAddFilter={addFilterHandler} />
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 <ExpenseItem
                     title={props.expenses[0].title}
                     amount={props.expenses[0].amount}
