381bc326140ac4cb156a439a54229a9dccfcd944
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-08-13 20:01:41 -0400

    add dynamic filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
@@ -8,35 +8,30 @@ import './Expenses.css';
 const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('2020');
-    
+
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
+        console.log(props.expenses);
+        console.log(selectedYear);
     }
+
+    const filteredExpenses = props.expenses.filter(expense => {
+        console.log(expense.date.getFullYear());
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                <ExpenseItem
-                    title={props.expenses[0].title}
-                    amount={props.expenses[0].amount}
-                    date={props.expenses[0].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={props.expenses[1].title}
-                    amount={props.expenses[1].amount}
-                    date={props.expenses[1].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={props.expenses[2].title}
-                    amount={props.expenses[2].amount}
-                    date={props.expenses[2].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={props.expenses[3].title}
-                    amount={props.expenses[3].amount}
-                    date={props.expenses[3].date}
-                ></ExpenseItem>
+                {filteredExpenses.map(expense =>
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date} />
+                )}
             </Card>
         </div>
     );
